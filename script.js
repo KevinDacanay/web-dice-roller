6938474129724d15a4753ca67b41f80fb8cbbfde
Kevin <kevin.dacanay27@gmail.com>
2026-02-14 23:02:37 -0600

    Committing script.js - initial commit of javascript for D&D Dice Roller

---
+++ script.js
@@ -0,0 +1,6 @@
+function rollDice(sides = 20) {
+    const roll = Math.floor(Math.random() * sides) + 1;
+
+    document.getElementById('dice-results').innerHTML =
+        `<div class="dice-result dice-rolling">${roll}</div>`;
+}
